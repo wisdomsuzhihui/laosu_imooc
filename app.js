6dd131ce60dc03d55aba3a5ddbcf41a7a223a616
suzhihui <wisdom.su@163.com>
2017-03-17 15:39:48 +0800

    init

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ var port = process.env.PORT || 3000
 var app = express()
 
 app.set('views', './views')
-app.set('views engine', 'jade')
+app.set('view engine', 'jade')
 app.listen(port)
 
 console.log('laosu website started on port ' + port)
+
+// index page
+
+app.get('/', function (req, res) {
+  res.render('index', {
+    title: '老苏'
+  })
+})
