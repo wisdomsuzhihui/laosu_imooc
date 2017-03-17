4468a6ef93808cfe1294abc299a1f28ade732f0a
suzhihui <wisdom.su@163.com>
2017-03-17 14:57:38 +0800

    laosu app init

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert('ok')
+var express = require('express')
+var port = process.env.PORT || 3000
+var app = express()
+
+app.set('views', './views')
+app.set('views engine', 'jade')
+app.listen(port)
+
+console.log('laosu website started on port ' + port)
